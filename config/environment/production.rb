3e4ef3ac1b7ba54fc4ccadc25a0a83b6507df55d
Dane Lowe <dane@avidonline.co.nz>
2014-05-24 18:03:17 +1200

    fix prod env file

--- config/environment/production.rb
+++ config/environment/production.rb
@@ -1,4 +1,4 @@
 configure do
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings settings.smtp_settings.symbolize_keys
+  ActionMailer::Base.smtp_settings = settings.smtp_settings.symbolize_keys
 end
