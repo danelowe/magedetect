4c685526ca7869e3fa6a2df7f2e9171b8f5cd1d7
Dane Lowe <dane@avidonline.co.nz>
2014-06-04 11:42:06 +1200

    added quick screen script

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,5 +5,5 @@ set :scm, :git
 set :linked_files, %w{config/secrets.yml}
 set :linked_dirs, %w{log tmp csvs}
 set :sidekiq_log, File.join(current_path, 'log', 'sidekiq.log')
-set :sidekiq_require, File.join(current_path, 'app', 'workers', 'sites_worker.rb')
+set :sidekiq_require, File.join(current_path, 'app', 'core.rb')
 set :deploy_via, :remote_cache
