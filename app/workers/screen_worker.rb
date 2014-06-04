acaf840c6c8a70f21417b8c35558d3b2dc2cac94
Dane Lowe <dane@avidonline.co.nz>
2014-06-04 12:02:06 +1200

    update results output

--- app/workers/screen_worker.rb
+++ app/workers/screen_worker.rb
@@ -6,10 +6,10 @@ class ScreenWorker
   def perform(url)
     site = Site.new(url)
     if Indicators::SkinUrl.run(site)
-      File.open('results', 'a') { |f| f.write(site.domain) }
+      File.open('results', 'a') { |f| f.write("\n"+site.domain) }
     else
       site = Site.new('www.'+url)
-      File.open('results', 'a') { |f| f.write(site.domain) } if Indicators::SkinUrl.run(site)
+      File.open('results', 'a') { |f| f.write("\n"+site.domain) } if Indicators::SkinUrl.run(site)
     end
   end
 end
